001fd4248206a0f0e9691e069408247c2d9d4706
Farhad Shikder <frd1952@gmail.com>
2022-06-16 21:17:23 +0600

    knex-paginate installed and setup

--- config/database.js
+++ config/database.js
@@ -1,8 +1,13 @@
 const mysql = require('mysql');
 const knexFile = require('../knexfile');
 const knex = require('knex');
+
+const { attachPaginate } = require('knex-paginate');
 // Create the connection pool. The pool-specific settings are the defaults
 
 const db = knex(knexFile);
+
+attachPaginate();
+
 
 module.exports = db;

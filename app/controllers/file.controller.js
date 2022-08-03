12b5e684eaa3a3d8bd8476567a24f829e70a7b5d
Farhad Shikder <frd1952@gmail.com>
2022-08-03 21:48:02 +0600

    Dynamic destination for file upload

--- app/controllers/file.controller.js
+++ app/controllers/file.controller.js
@@ -1,24 +1,37 @@
 const fs = require("fs");
 const upload = async (req, res) => {
     let files = req.files;
-    res.send(files.map(file=>file.filename));
+    res.json({
+        status: 'success',
+        files: files.map(file => ({name: file.filename, path: file.path.replace(/\\/g, "/")}))
+    });
 }
+
+const singleUpload = async (req, res) => {
+    let file = req.file;
+    res.json({
+        status: 'success', file: {
+            name: file.filename, path: file.path.replace(/\\/g, "/"),
+            size: file.size
+        },
+    });
+}
+
 const remove = async (req, res) => {
-    let {fileName} = req.params;
 
-    console.log('fileName', fileName);
+    let {file} = req.params;
 
-    fs.unlink('./uploads/'+fileName, (er)=> {
-        if(er) {
+    fs.unlink(file, (er) => {
+        if (er) {
             return res.json({status: 'failed', er: er});
         }
         return res.json({status: 'success'});
 
     });
-    
 }
 
 module.exports = {
     upload,
-    remove
+    remove,
+    singleUpload
 }
--- routes/file.route.js
+++ routes/file.route.js
@@ -4,14 +4,30 @@ const fileController = require('../app/controllers/file.controller');
 const multer = require('multer');
 
 const mime = require('mime-types')
+const fs = require("fs");
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, 'uploads/')
+
+        let {directory} = req.query;
+
+        let destination = 'uploads/';
+
+        if (directory) {
+            destination = destination + directory + '/';
+
+            if (!fs.existsSync(destination)) {
+                fs.mkdirSync(destination, {recursive: true})
+            }
+
+        }
+
+        cb(null, destination);
     },
     filename: function (req, file, cb) {
-        let ext = mime.extension(file.mimetype);
-        cb(null, file.originalname);
+        let splitted = file.originalname.split('.');
+        let ext = splitted[splitted.length - 1];
+        cb(null, splitted[0] + '_' + Date.now() + '.' + ext);
     }
 });
 
@@ -19,6 +35,8 @@ const upload = multer({storage: storage});
 
 router.post('/upload', upload.array('files'), fileController.upload);
 
-router.delete('/:fileName', fileController.remove);
+router.post('/upload_one', upload.single('file'), fileController.singleUpload)
+
+router.delete('/:file', fileController.remove);
 
 module.exports = router;

85eaf7213f1172f671fbbdae125b4a1f902fcb44
Farhad Shikder <frd1952@gmail.com>
2022-08-12 17:54:09 +0600

    Update Posts table

--- database/migrations/20220718105941_posts.js
+++ database/migrations/20220718105941_posts.js
@@ -1,17 +1,13 @@
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> }
- */
+
 exports.up = function (knex) {
     return knex.schema.createTable('posts', (table) => {
         table.increments();
         table.text('body');
         table.integer('userId').unsigned();
-        table.string('classId');
-        table.timestamps({useCamelCase: true, defaultToNow: true})
+        table.string('type');
 
-        table.foreign('classId').references('id').inTable('classes')
-            .onDelete('cascade');
+        table.string('typeId');
+        table.timestamps({useCamelCase: true, defaultToNow: true})
 
         table.foreign('userId').references('id').inTable('users')
             .onDelete('cascade');
@@ -19,10 +15,7 @@ exports.up = function (knex) {
     });
 };
 
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> }
- */
+
 exports.down = function (knex) {
     return knex.schema.dropTable('posts');
 };

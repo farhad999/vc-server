fa2a12f388158a0fab7568a56aafa9398e4a0826
Farhad Shikder <frd1952@gmail.com>
2022-09-01 14:00:18 +0600

    Create 20220831133658_answers_table.js

---
+++ database/migrations/20220831133658_answers_table.js
@@ -0,0 +1,19 @@
+exports.up = function(knex) {
+  return knex.schema.createTable('answers', table => {
+      table.string('id').primary();
+      table.text('body');
+      table.string('questionId');
+      table.integer('userId').unsigned();
+
+      table.foreign('questionId').references('id')
+          .inTable('questions').onDelete('cascade');
+
+      table.foreign('userId').references('id')
+          .inTable('users').onDelete('cascade');
+  })
+};
+
+
+exports.down = function(knex) {
+  return knex.schema.dropTable('answers');
+};

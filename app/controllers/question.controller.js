257d7291106c750f9bc66b1e24e4de54be7a4c64
Farhad Shikder <frd1952@gmail.com>
2022-09-03 17:29:05 +0600

    question with tag system

--- app/controllers/question.controller.js
+++ app/controllers/question.controller.js
@@ -1,29 +1,54 @@
 const db = require('../../config/database');
 const Joi = require("joi");
 const {faker} = require('@faker-js/faker')
-const Question = require('../models/Question');
-const Answer = require('../models/Answer')
 
 const index = async (req, res) => {
 
-    let {type, typeId, ref} = req.query;
+    let {type, typeId, ref, tag} = req.query;
 
     const user = req.user;
 
-    let page = 0;
+    let page = 1;
 
-    const questionQuery = Question.query().withGraphFetched('user')
-        .where('morphType', '=', type)
-        .where('morphId', '=', typeId)
-        .orderBy('createdAt', 'desc');
+    const questionQuery = db('questions')
+        .select('questions.id', 'questions.userId', 'questions.title', 'questions.createdAt',
+            cb => cb.from('answers').count('answers.id').where('questions.id', '=', db.ref('answers.questionId')).as('count')
+        )
+        .where('questions.morphType', '=', type)
+        .where('questions.morphId', '=', typeId)
+        .orderBy('questions.createdAt', 'desc');
+
 
     if (ref === 'my') {
         questionQuery.where('userId', '=', user.id)
     }
 
-    const questions = await questionQuery.page(page, 10);
+    if (tag) {
+        questionQuery.join('taggables', 'taggables.taggableId', '=', 'questions.id')
+            .join('tags', 'tags.id', '=', 'taggables.tagId')
+            .where('tags.slug', '=', tag);
+    }
 
-    return res.json(questions);
+    const {data, pagination} = await questionQuery.paginate({page: page, isLengthAware: true, perPage: 10});
+
+
+    const tags = await db('tags')
+        .select('tags.id', 'tags.name', 'tags.slug', 'taggables.taggableId')
+        .join('taggables', 'taggables.tagId', '=', 'tags.id')
+        .whereIn('taggables.taggableId', data.map(item => item.id));
+
+    const postUser = await db('users')
+        .select('id', 'firstName', 'lastName')
+        .whereIn('id', data.map(item => item.userId));
+
+    //now process this data
+
+    data.map((question) => {
+        question.tags = tags.filter(item => item.taggableId === question.id);
+        question.user = postUser.find(item => item.id === question.userId);
+    })
+
+    return res.json({data, pagination});
 }
 
 const store = async (req, res) => {
@@ -33,6 +58,10 @@ const store = async (req, res) => {
     const schema = Joi.object({
         title: Joi.string().required(),
         description: Joi.string(),
+        tags: Joi.array().items(Joi.object({
+            name: Joi.string().alphanum(),
+            slug: Joi.string().required(),
+        })),
         type: Joi.string(),
         typeId: Joi.string(),
     })
@@ -42,11 +71,17 @@ const store = async (req, res) => {
     if (!error) {
 
 
-        let {title, description, type, typeId} = value;
+        let {title, description, type, typeId, tags} = value;
+
+        const trxProvider = db.transactionProvider();
+        const trx = await trxProvider();
+
+        let questionId = faker.random.alphaNumeric(6)
 
         try {
-            await db('questions').insert({
-                id: faker.random.alphaNumeric(6),
+
+            await trx('questions').insert({
+                id: questionId,
                 title,
                 description,
                 morphType: type,
@@ -54,9 +89,46 @@ const store = async (req, res) => {
                 userId: user.id,
             });
 
+            for (const tag of tags) {
+
+                console.log('tag', tag);
+
+                let t = await trx('tags')
+                    .where('slug', '=', tag.slug)
+                    .where('morphType', '=', type)
+                    .where('morphId', '=', typeId)
+                    .first();
+
+                let tagId;
+
+                if (!t) {
+
+                    tagId = faker.random.alphaNumeric(6)
+
+                    await trx('tags').insert({
+                        ...tag,
+                        morphType: type,
+                        morphId: typeId,
+                        id: tagId
+                    })
+                } else {
+                    tagId = t.id;
+                }
+                await trx('taggables')
+                    .insert({
+                        id: faker.random.alphaNumeric(6),
+                        taggableType: 'questions',
+                        taggableId: questionId,
+                        tagId: tagId,
+                    })
+
+            }
+            trx.commit();
+
             return res.json({status: 'success', message: 'Question Posted'});
 
         } catch (er) {
+            trx.rollback();
             return res.status(500).send(er.message);
         }
 
@@ -68,15 +140,34 @@ const store = async (req, res) => {
 
 const viewQuestion = async (req, res) => {
     const {id} = req.params;
-    const question = await Question.query().withGraphFetched('user')
-        .findById(id);
+
+    const question = await db('questions')
+        .select('id', 'title', 'description', 'createdAt', 'userId')
+        .where('id', '=', id)
+        .first();
 
     if (!question) {
         return res.status(404).send('Not found');
     }
 
-    const answers = await Answer.query().withGraphFetched('user')
+    //now get the user related to this question
+
+    question.user = await db('users')
+        .select('id', 'firstName', 'lastName')
+        .where('id', '=', question.userId)
+        .first();
+
+    const answers = await db('answers')
         .where('questionId', '=', id);
+
+    const users = await db('users')
+        .select('id', 'firstName', 'lastName')
+        .whereIn('id', answers.map(item => item.userId));
+
+    answers.map((answer)=> {
+        answer.user = users.find(item=>item.id === answer.userId);
+        return answer;
+    })
 
     return res.json({question, answers});
 }

939dca4f262443f7db21a6555139c6b9e631a402
Farhad Shikder <frd1952@gmail.com>
2022-10-09 01:02:02 +0600

    start conversation

--- app/controllers/message.controller.js
+++ app/controllers/message.controller.js
@@ -1,5 +1,6 @@
 const db = require("../../config/database");
 const Joi = require("joi");
+const {nanoid} = require("nanoid");
 const getConversationList = async (req, res) => {
 
     const user = req.user;
@@ -68,12 +69,71 @@ const viewConversation = async (req, res) => {
         .orderBy('createdAt', 'desc')
         .paginate({perPage: 10, currentPage: page, isLengthAware: true});
 
-    return res.json({conversation,messageData:  messages})
+    return res.json({conversation, messageData: messages})
+}
+
+const start = async (req, res) => {
+
+    const {receiverId} = req.body;
+
+    const user = req.user;
+
+    const conversation = await db('conversations')
+        .join('conversation_participants as cp', 'cp.conversationId', '=', 'conversations.id')
+        .join('conversation_participants as cp2', 'cp2.conversationId', '=', 'cp.conversationId')
+        .where('conversations.type', '=', 'single')
+        .where('cp.userId', '=', user.id)
+        .where('cp2.userId', '=', receiverId)
+        .first();
+
+    if (!conversation) {
+
+
+        const id = nanoid();
+
+        const participants = [
+            {
+                id: nanoid(),
+                userId: user.id,
+                conversationId: id,
+            }, {
+                id: nanoid(),
+                userId: receiverId,
+                conversationId: id,
+            }
+        ]
+
+        const trxProvider = db.transactionProvider();
+
+        const trx = await trxProvider();
+
+        try {
+
+            await trx('conversations')
+                .insert({id, type: 'single'});
+
+            await trx('conversation_participants')
+                .insert(participants);
+
+            trx.commit();
+
+            return res.json({status: 'success', conversationId: id});
+
+        } catch (er) {
+            return res.status(500).json({message: er.message});
+        }
+
+    }
+
+    return res.json({status: 'success', conversationId: conversation.id});
+
+
 }
 
 module.exports = {
     sendMessage,
     getConversationList,
-    viewConversation
+    viewConversation,
+    start,
 
 }
--- routes/message.route.js
+++ routes/message.route.js
@@ -2,6 +2,8 @@ const auth = require("../app/middlewares/auth");
 const messageController = require('../app/controllers/message.controller')
 const router = require('express').Router();
 
+router.post('/start', auth, messageController.start);
+
 router.get('/:id',auth, messageController.viewConversation);
 
 router.post('/:id', auth, messageController.sendMessage);

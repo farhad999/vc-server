aa50d61c73ab8f147dc132cb8611ee1f8e0d765c
Farhad Shikder <frd1952@gmail.com>
2022-11-13 21:39:09 +0600

    auth with multiple guards

--- database/migrations/20220606145205_tokens_table.js
+++ database/migrations/20220606145205_tokens_table.js
@@ -1,25 +1,14 @@
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> }
- */
 exports.up = function(knex) {
   return knex.schema.createTable('tokens', (table)=> {
     table.increments();
     table.text('token').notNullable();
     table.integer('userId').unsigned();
+    table.string('guard').notNullable();
     table.boolean('isBlackListed').defaultTo(false);
     table.timestamps();
-
-    table.foreign('userId').references('id').inTable('users')
-        .onDelete('cascade');
-
   });
 };
 
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> }
- */
 exports.down = function(knex) {
   return knex.schema.dropTable('tokens');
 };

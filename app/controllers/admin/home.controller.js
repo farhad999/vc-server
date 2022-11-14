bcf4787ef5beccab60f92024b9f239b7243c6c2b
Farhad Shikder <frd1952@gmail.com>
2022-11-14 23:01:00 +0600

    admin dashboard

--- app/controllers/admin/home.controller.js
+++ app/controllers/admin/home.controller.js
@@ -1,5 +1,19 @@
-const index = (req, res) => {
-    res.json("from admin index")
+const db = require('../../../config/database')
+
+const index = async (req, res) => {
+
+    let userCounts = await db('users')
+        .select('userType', (cb)=>cb.count('id').as('count'))
+        .groupBy('userType');
+
+    let totalCourse = await db('courses')
+        .select((cb)=>cb.count('id').as('count'));
+
+    let totalSemesters = await db('semesters')
+        .select((cb)=>cb.count('id').as('count'));
+
+    res.json({counts: userCounts, totalCourses: totalCourse[0].count,
+        totalSemesters: totalSemesters[0].count})
 }
 
 module.exports = {
--- routes/admin/admin-api.route.js
+++ routes/admin/admin-api.route.js
@@ -1,6 +1,10 @@
 const router = require('express').Router();
 const authRoute = require('./auth.route');
+const auth = require('../../app/middlewares/auth')
+const homeController = require('../../app/controllers/admin/home.controller')
 
 router.use('/auth', authRoute);
+
+router.get('/', auth('admin'), homeController.index);
 
 module.exports = router;

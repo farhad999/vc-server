22439c34c39c7aa9b3238932c334032e26ac9637
Farhad Shikder <frd1952@gmail.com>
2022-12-11 13:59:23 +0600

    update class routines table

--- database/migrations/20221211085400_routine_class_times.js
+++ database/migrations/20221211085400_routine_class_times.js
@@ -3,14 +3,14 @@
  * @returns { Promise<void> }
  */
 exports.up = function(knex) {
-  return knex.schema.createTable('class_times', (table) => {
+  return knex.schema.createTable('routine_class_times', (table) => {
       table.increments();
-      table.string('classId').notNullable();
+      table.integer('routineClassId').unsigned();
       table.string('day').notNullable();
       table.time('startTime').notNullable();
       table.integer('periods').notNullable();
 
-      table.foreign('classId').references('id').inTable('classes').onDelete('cascade');
+      table.foreign('routineClassId').references('id').inTable('routine_classes').onDelete('cascade');
   })
 };
 

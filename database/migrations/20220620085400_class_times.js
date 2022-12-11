f720e8727a09a2f9e7c75964b82a3cbdf6899420
Farhad Shikder <frd1952@gmail.com>
2022-12-11 14:35:25 +0600

    Revert "update class routines table"
    This reverts commit 22439c34c39c7aa9b3238932c334032e26ac9637.

--- database/migrations/20220620085400_class_times.js
+++ database/migrations/20220620085400_class_times.js
@@ -3,14 +3,14 @@
  * @returns { Promise<void> }
  */
 exports.up = function(knex) {
-  return knex.schema.createTable('routine_class_times', (table) => {
+  return knex.schema.createTable('class_times', (table) => {
       table.increments();
-      table.integer('routineClassId').unsigned();
+      table.string('classId').notNullable();
       table.string('day').notNullable();
       table.time('startTime').notNullable();
       table.integer('periods').notNullable();
 
-      table.foreign('routineClassId').references('id').inTable('routine_classes').onDelete('cascade');
+      table.foreign('classId').references('id').inTable('classes').onDelete('cascade');
   })
 };
 

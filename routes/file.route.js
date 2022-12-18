d941277fa6384cfe33ef795a54caab83d6c741f0
Farhad Shikder <frd1952@gmail.com>
2022-12-18 20:05:19 +0600

    Update file directory

--- routes/file.route.js
+++ routes/file.route.js
@@ -1,42 +1,40 @@
-const router = require('express').Router();
-const fileController = require('../app/controllers/file.controller');
+const router = require("express").Router();
+const fileController = require("../app/controllers/file.controller");
 
-const multer = require('multer');
+const multer = require("multer");
 
-const mime = require('mime-types')
+const mime = require("mime-types");
 const fs = require("fs");
 
 const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
+  destination: function (req, file, cb) {
+    let { directory } = req.query;
 
-        let {directory} = req.query;
+    let destination = "";
 
-        let destination = 'uploads/';
+    if (directory) {
+      destination = destination + directory + "/";
 
-        if (directory) {
-            destination = destination + directory + '/';
-
-            if (!fs.existsSync(destination)) {
-                fs.mkdirSync(destination, {recursive: true})
-            }
-
-        }
-
-        cb(null, destination);
-    },
-    filename: function (req, file, cb) {
-        let splitted = file.originalname.split('.');
-        let ext = splitted[splitted.length - 1];
-        cb(null, splitted[0] + '_' + Date.now() + '.' + ext);
+      if (!fs.existsSync(destination)) {
+        fs.mkdirSync(destination, { recursive: true });
+      }
     }
+
+    cb(null, destination);
+  },
+  filename: function (req, file, cb) {
+    let splitted = file.originalname.split(".");
+    let ext = splitted[splitted.length - 1];
+    cb(null, splitted[0] + "_" + Date.now() + "." + ext);
+  },
 });
 
-const upload = multer({storage: storage});
+const upload = multer({ storage: storage });
 
-router.post('/upload', upload.array('files'), fileController.upload);
+router.post("/upload", upload.array("files"), fileController.upload);
 
-router.post('/upload_one', upload.single('file'), fileController.singleUpload)
+router.post("/upload_one", upload.single("file"), fileController.singleUpload);
 
-router.delete('/:file', fileController.remove);
+router.delete("/:file", fileController.remove);
 
 module.exports = router;
